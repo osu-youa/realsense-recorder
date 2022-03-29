155b6397dcc6109103761f90e6445a6b0e049fee
Cindy Grimm <cindy.grimm@oregonstate.edu>
2022-03-29 07:40:11 -0700

    Adding in name and number of seconds

--- record_video.py
+++ record_video.py
@@ -25,6 +25,7 @@ if not os.path.exists(output_dir):
     os.mkdir(output_dir)
 
 if __name__ == '__main__':
+    print("Connecting...")
     pipe = rs.pipeline()
     config = rs.config()
     config.enable_stream(rs.stream.color, 640, 480, rs.format.rgb8, 30)
@@ -40,48 +41,60 @@ if __name__ == '__main__':
         play_sound = lambda: None
 
     try:
-        while input('Type q to quit, Enter to continue: ').strip() != 'q':
-            print(f'Waiting {WAIT} seconds...')
-            time.sleep(WAIT)
-            play_sound()
-            while True:
-                path = os.path.join(output_dir, 'video_{}.avi'.format(counter))
-                if not os.path.exists(path):
-                    break
+        print("Recording...")
+        done = False
+        name = "video"
+        while done is False:
+            query = input('Type q to quit, n to change name, t for changing time, Enter to continue: ').strip()
+            if query is 'q':
+                done = True
+            elif query is 'n':
+                name = input("Type name: ")
+            elif query is 't':
+                VIDEO_LENGTH = int(input("Time in seconds: "))
+            else:
+                print(f'Waiting {WAIT} seconds...')
+                time.sleep(WAIT)
+                play_sound()
+                while True:
+                    path = os.path.join(output_dir, '{}_{}.avi'.format(name, counter))
+                    if not os.path.exists(path):
+                        break
+                    counter += 1
+
+                format = cv2.VideoWriter_fourcc(*'MPEG')
+                out = cv2.VideoWriter(path, format, 30, (1280, 480))
+                start = time.time()
+                count_frames = 0
+                save_depth = []
+                while time.time() - start < VIDEO_LENGTH:
+                    frames = pipe.wait_for_frames()
+                    depth_frame = frames.get_depth_frame()
+                    color_frame = frames.get_color_frame()
+                    if not depth_frame or not color_frame:
+                        continue
+
+                    # Convert images to numpy arrays
+                    count_frames += 1
+                    depth_image = np.asanyarray(depth_frame.get_data())
+                    color_image = np.asanyarray(color_frame.get_data())
+
+                    # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
+                    depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+                    save_depth.append([int(np.min(depth_image)), int(np.max(depth_image))])
+
+                    # Stack both images horizontally
+                    images = np.hstack((color_image, depth_colormap))
+                    #rgb_img = np.asanyarray(frames.get_color_frame().get_data())
+                    out.write(cv2.cvtColor(images, cv2.COLOR_RGB2BGR))
+
+                fname = f"videos/{name}_depth_{counter}.json"
+                with open(fname, 'w') as f:
+                    json.dump(save_depth, f)
                 counter += 1
+                out.release()
+                play_sound()
+                print('Output to {}, {} frames'.format(path, count_frames))
 
-            format = cv2.VideoWriter_fourcc(*'MPEG')
-            out = cv2.VideoWriter(path, format, 30, (1280, 480))
-            start = time.time()
-            count_frames = 0
-            save_depth = []
-            while time.time() - start < VIDEO_LENGTH:
-                frames = pipe.wait_for_frames()
-                depth_frame = frames.get_depth_frame()
-                color_frame = frames.get_color_frame()
-                if not depth_frame or not color_frame:
-                    continue
-
-                # Convert images to numpy arrays
-                count_frames += 1
-                depth_image = np.asanyarray(depth_frame.get_data())
-                color_image = np.asanyarray(color_frame.get_data())
-
-                # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
-                depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
-                save_depth.append([int(np.min(depth_image)), int(np.max(depth_image))])
-
-                # Stack both images horizontally
-                images = np.hstack((color_image, depth_colormap))
-                #rgb_img = np.asanyarray(frames.get_color_frame().get_data())
-                out.write(cv2.cvtColor(images, cv2.COLOR_RGB2BGR))
-
-            fname = "videos/depth_{0}.json".format(counter)
-            with open(fname, 'w') as f:
-                json.dump(save_depth, f)
-            counter += 1
-            out.release()
-            play_sound()
-            print('Output to {}, {} frames'.format(path, count_frames))
     finally:
         pipe.stop()
